5abf4675f47169ae14b89a2cfb50d5d76a588856
PedroJSDev <pjsoares46@gmail.com>
2021-11-12 13:20:21 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import Router from 'svelte-spa-router'
+	import Home from './routes/Home.svelte'
+	import Contact from './routes/Contact.svelte'
+	
+	let routes = {
+		'/': Home,
+		'/contact': Contact
+	}
+
+	let svelte: string
+	let typescript: string
+	let sass: string
+
+	svelte = 'Svelte'
+	typescript = 'Typescript'
+	sass = 'Sass'
+</script>
+
+<div class="app">
+	<nav>
+		<a href="/#/">Home</a>
+		<a href="/#/contact">Contact</a>
+	</nav>
+
+	<Router {routes} />
+</div>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	let svelte: string
+	let typescript: string
+	let sass: string
+
+	svelte = 'Svelte'
+	typescript = 'Typescript'
+	sass = 'Sass'
+
+</script>
+
+<div class="home">
+  <h3>Home Page</h3>
+  <h3 class="home__svelte">Hello {svelte}</h3>
+  <h3 class="home__typescript">Hello {typescript}</h3>
+  <h3 class="home__sass">Hello {sass}</h3>
+</div>
+
+<style lang="scss">
+	$svelte-color: #ff3e00;
+	$typescript-color: #3178c6;
+	$sass-color: #cf649a;
+
+	.home {
+		&__svelte {
+			color: $svelte-color;
+		}
+
+		&__typescript {
+			color: $typescript-color;
+		}
+
+		&__sass {
+			color: $sass-color;
+		}
+	}
+</style>
